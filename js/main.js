66ef521dc4f40ef59f35d4535b790e49912b51dc
kasumi2810 <148180155+kasumi2810@users.noreply.github.com>
2023-11-01 23:07:10 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,87 @@
-
-// 穴埋め形式です。空いている箇所を埋めてスクーススクロールを実装してください
 $(function () {
   /*=================================================
-  スムーススクロール
-  ===================================================*/
-  // ページ内のリンクをクリックした時に動作する
-  $('a[href^="#"]').click(function () {
-    // クリックしたaタグのリンクを取得
-    let href = $(this).attr("href");
-    // ジャンプ先のid名をセット hrefの中身が#もしくは空欄なら,htmlタグをセット
-    let target = $(href == "#" || href == "" ? "html" : href);
-    // ページトップからジャンプ先の要素までの距離を取得
-    let position = target.offset().top;
-    // animateでスムーススクロールを行う   ページトップからpositionだけスクロールする
-    // 600はスクロール速度で単位はミリ秒  swingはイージングのひとつ
-    $("html, body").animate({ scrollTop: position }, 1000 , "swing");
-    // urlが変化しないようにfalseを返す
-    return false;
+    ハンバーガ―メニュー
+    ===================================================*/
+  // ハンバーガーメニューをクリックした時
+  $(".hamburger").on("click", function () {
+    $("#header").toggleClass("open");
   });
-
+  // メニューのリンクをクリックした時
+  $("#nav a").on("click", function () {
+    $("#header").toggleClass("open");
+  });
 });
 
+$(window).scroll(function () {
+  // fadeinクラスに対して順に処理を行う
+  $(".fadein").each(function () {
+    // スクロールした距離
+    let scroll = $(window).scrollTop();
+    // fadeinクラスの要素までの距離
+    let target = $(this).offset().top;
+    // 画面の高さ
+    let windowHeight = $(window).height();
+    // fadeinクラスの要素が画面下にきてから200px通過した
+    // したタイミングで要素を表示
+    if (scroll > target - windowHeight + $(this).height() / 2) {
+      $(this).css("opacity", "1");
+      $(this).css("transform", "translateY(0)");
+    }
+  });
+});
 
+$(window).scroll(function () {
+  // fadeinクラスに対して順に処理を行う
+  $(".fadein2").each(function () {
+    // スクロールした距離
+    let scroll = $(window).scrollTop();
+    // fadeinクラスの要素までの距離
+    let target = $(this).offset().top;
+    // 画面の高さ
+    let windowHeight = $(window).height();
+    // fadeinクラスの要素が画面下にきてから200px通過した
+    // したタイミングで要素を表示
+    if (scroll > target - windowHeight + $(this).height() / 2) {
+      $(this).css("opacity", "1");
+      $(this).css("transform", "translateY(0)");
+    }
+  });
 
+  function fadeAnime() {
+    // flipLeft
+    $(".gallery-wrapper li").each(function () {
+      var elemPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll >= elemPos - windowHeight) {
+        $(this).addClass("flipLeft");
+      } else {
+        $(this).removeClass("flipLeft");
+      }
+    });
+  }
 
+  // 画面をスクロールをしたら動かしたい場合の記述
+  $(window).scroll(function () {
+    fadeAnime();
+  });
+});
+
+$(".slider").slick({
+  autoplay: true, //自動的に動き出すか。初期値はfalse。
+  infinite: true, //スライドをループさせるかどうか。初期値はtrue。
+  slidesToShow: 2, //スライドを画面に2枚見せる
+  slidesToScroll: 2, //1回のスクロールで2枚の写真を移動して見せる
+  prevArrow: '<div class="slick-prev"></div>', //矢印部分PreviewのHTMLを変更
+  nextArrow: '<div class="slick-next"></div>', //矢印部分NextのHTMLを変更
+  dots: true, //下部ドットナビゲーションの表示
+  responsive: [
+    {
+      breakpoint: 769, //モニターの横幅が769px以下の見せ方
+      settings: {
+        slidesToShow: 1, //スライドを画面に1枚見せる
+        slidesToScroll: 1, //1回のスクロールで1枚の写真を移動して見せる
+      },
+    },
+  ],
+});
